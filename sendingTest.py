fb3e1b5dd3d506d75e87e83c8ab10688758c2a3e
Daniil <m1802288@edu.misis.ru>
2019-07-16 12:59:03 +0100

    V1

--- sendingTest.py
+++ sendingTest.py
@@ -8,7 +8,9 @@ bus = can.interface.Bus(channel=channel, bustype=bustype)
 msg = bus.recv()
 print(map(ord, msg.data))
 print(map(ord, msg.arbitration_id))
-stop = can.Message(arbitration_id= 0c000000)
+
+stop = can.Message(arbitration_id=0x0c000000, is_extended_id=False)
+
 bus.send(stop)
 bus.send(stop)
 
@@ -16,5 +18,5 @@ bus.send(stop)
 
 #str = build_frame("181C0100#105a205b00000000")
 time.sleep(10)
-#msg = can.Message(arbitration_id=0x0c040100, is_extended_id=False)
+msg = can.Message(arbitration_id=0x0c040100, is_extended_id=False)
 bus.send(msg)
