2a56b1b9ef7c3aee09657cda6dffa22faf3179ff
Daniil <m1802288@edu.misis.ru>
2019-07-16 12:55:07 +0100

    V1

--- sendingTest.py
+++ sendingTest.py
@@ -1,7 +1,20 @@
-import socket, sys, os, struct, array, threading
-from time import *
-from fcntl import ioctl
+import time
+import can
 from can2RNET import *
-cansendtxt = "0C000402#"
-out=build_frame(cansendtxt)
-print (struct.unpack('B',out[:1]))
+bustype = 'socketcan'
+channel = 'can0'
+
+bus = can.interface.Bus(channel=channel, bustype=bustype)
+msg = bus.recv()
+print(map(ord, msg.data))
+print(map(ord, msg.arbitration_id))
+stop = can.Message(arbitration_id= 0c000000)
+bus.send(stop)
+bus.send(stop)
+
+bus.send(stop)
+
+#str = build_frame("181C0100#105a205b00000000")
+time.sleep(10)
+#msg = can.Message(arbitration_id=0x0c040100, is_extended_id=False)
+bus.send(msg)
