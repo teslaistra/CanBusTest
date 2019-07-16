e9958bb4c9996d288c85c83ad138ee8ee14fc80f
Daniil <m1802288@edu.misis.ru>
2019-07-16 14:02:04 +0100

    V1

--- sendingTest.py
+++ sendingTest.py
@@ -8,13 +8,13 @@ bus = can.interface.Bus(channel=channel, bustype=bustype)
 msg = bus.recv()
 #print(map(ord, msg.data))
 #print(map(ord, msg.arbitration_id))
-print(msg.data)
-stop = can.Message(arbitration_id=0x0c000000, is_extended_id=False)
+print(msg.arbitration_id)
+#stop = can.Message(arbitration_id=0x0c000000, is_extended_id=False)
 
-bus.send(stop)
-bus.send(stop)
+#bus.send(stop)
+#bus.send(stop)
 
-bus.send(stop)
+#bus.send(stop)
 
 #str = build_frame("181C0100#105a205b00000000")
 
