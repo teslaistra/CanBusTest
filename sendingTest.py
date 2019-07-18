70ca5d54cd420e9e697752a072a7e7b739d7d7fb
Daniil <m1802288@edu.misis.ru>
2019-07-18 14:51:02 +0100

    V1

--- sendingTest.py
+++ sendingTest.py
@@ -44,7 +44,7 @@ while 0 == 0:
             sleep(0.01)
             a = array.array('B', build_frame("#9c"))
             print(build_frame("#9c"))
-            print (map(ord,build_frame("#9c")))
+            print(binascii.hexlify(build_frame("#9c")))
             b = msg.data
             if a == b: print("true")
             msg1 = can.Message(arbitration_id=33554432, data=a, is_extended_id=True)
