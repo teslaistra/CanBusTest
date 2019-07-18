358d9e2d876d1b49a0472703238090755726c521
Daniil <m1802288@edu.misis.ru>
2019-07-18 16:37:25 +0100

    V1

--- sendingTest.py
+++ sendingTest.py
@@ -41,14 +41,14 @@ while 0 == 0:
 
         print("error inducing")
 
-        #induce_JSM_error(bus)
+        induce_JSM_error(bus)
         while t > time():
 
 
             sleep(0.001)
             a1 = build_frame("#9c")
             a = array.array('B', a1)
-            msg = can.Message(data=a)
+            msg.data = a
             bus.send(msg)
 
 
