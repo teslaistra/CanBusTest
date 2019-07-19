8274b445e3c469cb34699c3273307d2380435d43
Daniil <m1802288@edu.misis.ru>
2019-07-19 11:20:45 +0100

    V1

--- sendingTest.py
+++ sendingTest.py
@@ -12,6 +12,9 @@ msg = bus.recv()
 def induce_JSM_error(cansocket):
     for i in range(0, 3):
         cansend(cansocket, '0c000000#')
+while 0 == 0:
+    msg = bus.recv()
+    print(binascii.hexlify(msg.data)[0:2])
 
 
 msgR = can.Message(arbitration_id =33554432, data = bytearray([0,156]), extended_id = True)
