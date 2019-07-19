a09188d3dae1a2aa40e7542a08e7a851e3efa905
Daniil <m1802288@edu.misis.ru>
2019-07-19 10:11:43 +0100

    V1

--- sendingTest.py
+++ sendingTest.py
@@ -15,10 +15,12 @@ def induce_JSM_error(cansocket):
 
 
 
-while 0 ==0:
-    msg = bus.recv()
 
-    if (msg.arbitration_id == 33554432):
+msg = bus.recv()
+
+if (msg.arbitration_id == 33554432):
+      while 0==0:
+
         if binascii.hexlify(msg.data)[0:2] == "9c":
             print ("got!")
 
