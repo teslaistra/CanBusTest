dbef4e899138156273a5f0fee6a85d29ef96299c
Daniil <m1802288@edu.misis.ru>
2019-07-22 16:22:48 +0100

    V2

--- sendingTest.py
+++ sendingTest.py
@@ -16,5 +16,6 @@ list =['33554432','472908036','470548736', '63115023', '338690304','202637824','
 while 0==0:
     msg = bus.recv()
     if str(msg.arbitration_id) not in list:
+        print()
         print(str(msg.arbitration_id))
         print(binascii.hexlify(msg.data)[0:2])
