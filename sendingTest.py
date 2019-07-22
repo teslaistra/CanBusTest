6016c0a291d23886b5bc88945291534384a0361e
Daniil <m1802288@edu.misis.ru>
2019-07-22 16:34:39 +0100

    V2

--- sendingTest.py
+++ sendingTest.py
@@ -12,10 +12,10 @@ msg = bus.recv()
 def induce_JSM_error(cansocket):
     for i in range(0, 3):
         cansend(cansocket, '0c000000#')
-list =['33554432','472908036','470548736', '63115023', '338690304','202637824','202637568','14']
+list =['472908036','470548736', '63115023', '338690304','202637824','202637568','14']#33554432 - id of JSM
 while 0==0:
     msg = bus.recv()
-    if str(msg.arbitration_id) not in list:
+    if str(msg.arbitration_id) not in list and binascii.hexlify(msg.data)!="0000":
         print()
         print(str(msg.arbitration_id))
         print(binascii.hexlify(msg.data))
