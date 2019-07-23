20430759b24bcddd75d9104ee47918ebc262a40b
Daniil <m1802288@edu.misis.ru>
2019-07-23 15:09:43 +0100

    V3

--- readingTest.py
+++ readingTest.py
@@ -14,28 +14,53 @@ bus = can.interface.Bus(channel=channel, bustype=bustype)
 def print_thread(bus):
     print('sf')
 
-    global t
+    global running
     global message_names
     global b
     print('sf')
-    while t:
-        print("aa")
-    msg = bus.recv()
-    a = message_names.get(str(msg.arbitration_id))
-    list = ['472908036', '470548736', '63115023', '338690304', '202637824', '202637568', '14', '33554432']
-    if  str(msg.arbitration_id) not in list:
-        print('recieved : ')
-        print(message_names[str(msg.arbitration_id)])
-        print('with data: ')
-        print(binascii.hexlify(msg.data))
-        print(' ')
-    elif a == None:
-        print('Adding')
-        d1 = {str(msg.arbitration_id) : 'new'}
-        print(d1)
-        print(' ')
-        message_names.update(d1)
+    while running:
 
+        msg = bus.recv()
+        a = message_names.get(str(msg.arbitration_id))
+        list = ['472908036', '470548736', '63115023', '338690304', '202637824', '202637568', '14', '33554432']
+        if a != None:
+            print('recieved : ')
+            print(message_names[str(msg.arbitration_id)])
+            print('with data: ')
+            print(binascii.hexlify(msg.data))
+            print(' ')
+        elif a == None:
+            print('Adding')
+            d1 = {str(msg.arbitration_id) : 'new'}
+            print(d1)
+            print(' ')
+            message_names.update(d1)
+
+
+periodic_messages = {
+'472908036' : 'distance counter',
+    '470548736': 'battery counter',
+    '63115023': 'device heartbeat',
+    '63115023': 'device heartbeat',
+    '338690304': 'PMtx drive motor current',
+    '202637824': 'PMtx heartbeart(??)',
+    '202637568': 'PMtx heartbeart(??)',
+    '14': 'bitmap of set lamp indicators on JSM',
+}
+
+controlling_messages = {
+    '33554432': 'drive control',
+    '404488192': 'beep',
+    '96': 'joy mode',
+    '97': 'smth connected with joy mode 1',
+    '202899968': 'seen after mode change - 1',
+    '202899969': 'seen after mode change - 2',
+    '98': 'smth connected with joy mode 2',
+}
+
+drive_messages = {
+    '33554432': 'drive control'
+}
 
 
 
@@ -61,12 +86,14 @@ message_names = {
 
 
 if __name__ == "__main__":
-    global t
-    t = True
-    print("dd")
-    g = [1,2,3]
+    global running
+    running = True
     global bus
+    global  message_names
     read_thread = threading.Thread(target=print_thread, args=(bus,))
     read_thread.start()
-    print(threading.active_count())
-    print("sd")
+    time1 = time() + 20
+    while time() < time1:
+        running = True
+    running = False
+    print(message_names)
