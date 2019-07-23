2c6a32c18dc85fffda69bd0b35ac304c6ad26dce
Daniil <m1802288@edu.misis.ru>
2019-07-23 11:55:39 +0100

    V3

--- sendingTest.py
+++ sendingTest.py
@@ -2,6 +2,16 @@ import time
 import can
 from can2RNET import *
 
+
+def dec2hex(dec, hexlen):  # convert dec to hex with leading 0s and no '0x'
+    h = hex(int(dec))[2:]
+    l = len(h)
+    if h[l - 1] == "L":
+        l -= 1  # strip the 'L' that python int sticks on
+    if h[l - 2] == "x":
+        h = '0' + hex(int(dec))[1:]
+    return ('0' * hexlen + h)[l:l + hexlen]
+print(dec2hex(96,16))
 bustype = 'socketcan'
 channel = 'can0'
 import binascii
