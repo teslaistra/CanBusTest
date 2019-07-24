45a5606d700c75777d5055a6089719a465832f13
Daniil <m1802288@edu.misis.ru>
2019-07-24 10:23:13 +0100

    V3

--- readingTest.py
+++ readingTest.py
@@ -11,19 +11,30 @@ import array
 global bus
 bus = can.interface.Bus(channel=channel, bustype=bustype)
 
+
+def dec2hex(self, dec, hexlen):  # convert dec to hex with leading 0s and no '0x'
+    h = hex(int(dec))[2:]
+    l = len(h)
+    if h[l - 1] == "L":
+        l -= 1  # strip the 'L' that python int sticks on
+    if h[l - 2] == "x":
+        h = '0' + hex(int(dec))[1:]
+    return ('0' * hexlen + h)[l:l + hexlen]
+
 def print_thread(bus):
     global running
     while running:
         msg = bus.recv()
-        a = message_names.get(str(msg.arbitration_id))
+        a = message_names.get(str(dec2hex(msg.arbitration_id,8)))
 
         if a != None:
             print('recieved : ')
-            #print(message_names[str(msg.arbitration_id)])
+            print(message_names[str(dec2hex(msg.arbitration_id))])
             #print('with data: ')
             #print(binascii.hexlify(msg.data))
             #print(' ')
         elif a == None:
+
             print('Adding')
             d1 = {str(msg.arbitration_id) : 'new'}
             print(d1)
@@ -39,7 +50,7 @@ periodic_messages = {
     '338690304': 'PMtx drive motor current',
     '202637824': 'PMtx heartbeart(??)',
     '202637568': 'PMtx heartbeart(??)',
-    '14': 'bitmap of set lamp indicators on JSM',
+    '14': 'bitmap of set lamp indicators on JSM'
 }
 
 controlling_messages = {
@@ -49,16 +60,18 @@ controlling_messages = {
     '97': 'smth connected with joy mode 1',
     '202899968': 'seen after mode change - 1',
     '202899969': 'seen after mode change - 2',
-    '98': 'smth connected with joy mode 2',
+    '98': 'smth connected with joy mode 2'
 }
 
 drive_messages = {
     '33554432': 'drive control'
 }
 
-
-
 message_names = {
+
+}
+
+message_names1 = {
     '472908036' : 'distance counter',
     '470548736': 'battery counter',
     '63115023':'device heartbeat',
